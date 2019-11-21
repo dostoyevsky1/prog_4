03d15bb911ae32ec69a2989e5498e00ee89f0700
dostoyevsky1 <mdrozdov94@gmail.com>
2019-11-21 14:13:43 -0500

    instantiated main() host file and function helper + header. defined power, sqrt, abs value funcs

---
+++ sort_helper.cpp
@@ -0,0 +1,59 @@
+// Absolute value function
+double abs(double val)
+{
+    if(val < 0)
+    {
+        return (-val);
+    } else 
+    {
+        return val;
+    }
+}
+
+// Square Root Function
+double Sqrt(double v, int initial_guess = 1)
+{
+    double new_val = 0.5*(initial_guess + v / initial_guess);
+    double error;
+
+    do
+    {
+        new_val = 0.5*(new_val + v / new_val);
+        error = 0.5*(new_val + v/new_val) - new_val;
+
+    } while (abs(error) > 0.005);
+
+    return new_val;
+    
+}
+
+// Power Function
+double Power(double b, int e)
+{
+    double final;
+    
+    final = b;
+
+    if(e == 0)
+    {
+        return 1;
+
+    } else if(e > 0)
+    {
+        for (int i = 0; i < (e-1); i++)
+        {
+            final *= b;
+        }
+
+    } else if(e < 0)
+    {
+        for (int i = 0; i < ((-e)-1); i++)
+        {
+            final *= b;
+        }
+        final = 1/final;
+    }
+
+    return final; 
+
+}
---
+++ sort_helper.h
@@ -0,0 +1,9 @@
+// Absolute value function
+double abs(double val);
+
+// Square Root Function
+double Sqrt(double v, int initial_guess = 1);
+
+// Power Function
+double Power(double b, int e);
+
