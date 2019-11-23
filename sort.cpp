e091d26904c874a0195c3158896b1a3e9c507c26
dostoyevsky1 <mdrozdov94@gmail.com>
2019-11-23 14:58:08 -0500

    instantiated user input for ascending or descending sort and table output

--- sort.cpp
+++ sort.cpp
@@ -7,6 +7,7 @@ int main()
 double l0[100];
 
 int size = GetData(l0);
+char order;
 
 double l1[size];
 
@@ -16,6 +17,23 @@ for(int i = 0; i < size; i++)
     
 }
 
+std::cout << std::endl <<
+             "Would you like to sort in Ascending(A/a) or Descending(D,d) order?" <<
+             std::endl;
+std::cin >> order;
+Sort(l1, size, order);
+
+std::cout << std::endl << "N" << "\t" << "Square" << "\t" << "Square Root" << std::endl;
+
+for(int i = 0; i < size; i++)
+{
+     std::cout << l1[i] << 
+                  "\t" <<
+                  Power(l1[i],2) <<
+                  "\t" <<
+                  Sqrt(l1[i]) <<
+                  std::endl;
+}
 
     return 0;
 }
